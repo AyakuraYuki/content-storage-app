ecfa7acbdd8b1ddfc680cafbbbb1dc9b98b64cef
Ayakura Yuki <AyakuraYuki@users.noreply.github.com>
2018-02-06 18:45:41 +0800

    remove useless class Global

--- src/main/java/cc/ayakurayuki/contentstorage/common/base/BaseBean.groovy
+++ src/main/java/cc/ayakurayuki/contentstorage/common/base/BaseBean.groovy
@@ -4,5 +4,5 @@ package cc.ayakurayuki.contentstorage.common.base
  * Created by ayakurayuki on 2018/1/16-11:21. <br/>
  * Package: cc.ayakurayuki.contentstorage.common.base <br/>
  */
-class Global {
+class BaseBean {
 }
