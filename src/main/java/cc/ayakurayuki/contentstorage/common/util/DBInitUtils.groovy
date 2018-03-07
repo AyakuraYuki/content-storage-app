57e9c02644573d122e8918bf5d3ffee6531a6990
Ayakura Yuki <AyakuraYuki@users.noreply.github.com>
2018-03-07 17:34:25 +0800

    DB init utils

---
+++ src/main/java/cc/ayakurayuki/contentstorage/common/util/DBInitUtils.groovy
@@ -0,0 +1,58 @@
+package cc.ayakurayuki.contentstorage.common.util
+
+import org.yaml.snakeyaml.Yaml
+
+import java.sql.DriverManager
+
+/**
+ * Created by ayakurayuki on 2018/3/7-15:35
+ * <p>
+ * Package: cc.ayakurayuki.contentstorage.common.util
+ */
+class DBInitUtils {
+
+    static def dbInit() {
+        // Get Spring Boot application configure file.
+        def filepath = DBInitUtils.class.classLoader.getResource('application.yml').file
+        // Transfer configure file to YAML object.
+        def yaml = new Yaml()
+        def config = yaml.loadAs(new File(filepath).newReader(), Map)
+        // Get JDBC URL config.
+        def jdbcURL = config['spring']['datasource']['url'] as String
+        def absolutePath = jdbcURL.replace(jdbcURL.substring(0, jdbcURL.lastIndexOf(':') + 1), "${System.getProperty("user.dir")}/")
+        def dbFile = new File(absolutePath)
+        if (dbFile.exists()) {
+            println """
+[DBInitUtils] SQLite database file is exists.
+[DBInitUtils] Database initialization completed.
+            """.trim()
+        } else {
+            def dbPath = absolutePath.substring(0, absolutePath.lastIndexOf('/') + 1)
+            new File(dbPath).mkdirs()
+            dbFile.createNewFile()
+            Class.forName('org.sqlite.JDBC')
+            def connection = DriverManager.getConnection(jdbcURL)
+            def statement = connection.createStatement()
+            def result = statement.execute("""
+            create table content (
+              id        text primary key,
+              item      text not null,
+              json_data text
+            );
+            
+            create table `settings` (
+              id    text primary key,
+              key   text not null,
+              value text
+            );
+            """.trim())
+            println """
+[DBInitUtils] SQLite database file is created. Statement result is ${result}.
+[DBInitUtils] Database initialization completed.
+            """.trim()
+            statement.close()
+            connection.close()
+        }
+    }
+
+}
---
+++ src/test/java/Playground.groovy
@@ -0,0 +1,16 @@
+import cc.ayakurayuki.contentstorage.common.util.DBInitUtils
+import org.junit.Test
+
+/**
+ * Created by ayakurayuki on 2018/3/7-15:40
+ * <p>
+ * Package: 
+ */
+class Playground {
+
+    @Test
+    void test() {
+        DBInitUtils.dbInit()
+    }
+
+}
