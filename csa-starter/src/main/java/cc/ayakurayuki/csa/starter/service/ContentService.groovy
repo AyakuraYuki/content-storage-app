e030b07c8744813fcd6c6b831c46a351dcd716df
Ayakura Yuki <AyakuraYuki@users.noreply.github.com>
2019-07-27 16:37:00 +0800

    Feat: C0nT3nt Service ;-D

--- csa-starter/src/main/java/cc/ayakurayuki/csa/starter/service/ContentService.groovy
+++ csa-starter/src/main/java/cc/ayakurayuki/csa/starter/service/ContentService.groovy
@@ -2,10 +2,13 @@ package cc.ayakurayuki.csa.starter.service
 
 import cc.ayakurayuki.csa.starter.core.config.Constants
 import cc.ayakurayuki.csa.starter.core.entity.Content
+import cc.ayakurayuki.csa.starter.core.util.IdUtils
 import cc.ayakurayuki.csa.starter.dao.ContentDao
+import cc.ayakurayuki.csa.starter.util.DesUtils
 import io.vertx.core.Future
 import io.vertx.core.json.Json
 import io.vertx.core.json.JsonObject
+import org.apache.commons.lang3.StringUtils
 import org.slf4j.LoggerFactory
 
 /**
@@ -25,9 +28,67 @@ class ContentService extends BaseService {
     Future.<List<JsonObject>> future { f ->
       contentDao.search keyword, f
     }.compose { ar ->
-      def list = ar.collect { object -> Json.decodeValue object.encode(), Content.class }
+      def list = ar.collect { object ->
+        final def content = Json.decodeValue object.encode(), Content.class
+        // Remove jsonData because this field is never used in list view.
+        content.jsonData = ''
+        return content
+      }
       Future.succeededFuture list
     }
+  }
+
+  Future<Content> get(String id) {
+    Future.<JsonObject> future { f ->
+      contentDao.get id, f
+    }
+        .compose { ar -> Future.succeededFuture Json.decodeValue(ar.encode(), Content.class) }
+        .compose { content ->
+          DesUtils.decryptFuture(content.jsonData)
+              .compose { json ->
+                content.jsonData = json
+                Future.succeededFuture content
+              }
+        }
+  }
+
+  Future<Integer> save(String id, String item, String jsonData) {
+    get(id)
+        .compose { content ->
+          if (null == content) {
+            content = new Content()
+            content.id = IdUtils.UUID()
+          }
+          if (StringUtils.isNotEmpty(item)) {
+            content.item = item
+          }
+          return Future.<Content> succeededFuture(content)
+        }
+        .compose { content ->
+          if (StringUtils.isNotEmpty(jsonData)) {
+            return DesUtils.encryptFuture(jsonData)
+                .compose { data ->
+                  content.jsonData = data
+                  return Future.<Content> succeededFuture(content)
+                }
+          } else {
+            return Future.<Content> succeededFuture(content)
+          }
+        }
+        .compose { content ->
+          return Future.<Integer> future { f -> contentDao.save content, f }
+        }
+  }
+
+  Future<Integer> delete(String id) {
+    get(id)
+        .compose { content ->
+          if (null == content) {
+            return Future.succeededFuture(0)
+          } else {
+            return Future.<Integer> future { f -> contentDao.delete id, f }
+          }
+        }
   }
 
 }
